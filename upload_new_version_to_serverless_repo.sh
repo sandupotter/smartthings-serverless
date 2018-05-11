775c31d97c299df979493577a02ee2670e812a46
Sandu Potter <sandupotter@gmail.com>
2018-05-11 17:15:33 +0100

    SAM deployment script changes

--- upload_new_version_to_serverless_repo.sh
+++ upload_new_version_to_serverless_repo.sh
@@ -26,6 +26,7 @@ python $BASEDIR/setup.py ldist
 SAM_TEMPLATE=$BASEDIR/$1
 SAM_TEMPLATE_OUTPUT=$BASEDIR/$2
 APPLICATION_ID=$3
+SOURCE_URL=$4
 NEXT_TAG=$(get_next_tag)
 LATEST_TAG=`git describe --abbrev=0 --tags`
 
@@ -33,7 +34,7 @@ if [ $# -eq 3 ]
 then
   SEMANTIC_VERSION=$NEXT_TAG
 else
-  SEMANTIC_VERSION=$4
+  SEMANTIC_VERSION=$5
 fi
 
 if [ $LATEST_TAG != $SEMANTIC_VERSION ]
@@ -45,11 +46,14 @@ fi
 
 AWS_SEMANTIC_VERSION=`echo $SEMANTIC_VERSION | cut -c 2-`
 
+SOURCE_URL=`echo $SOURCE_URL | sed -e "s/version/$SEMANTIC_VERSION/g"`
+
+
 aws cloudformation package --template-file $SAM_TEMPLATE --s3-bucket sandupotter-smart-things-serverless --output-template-file $SAM_TEMPLATE_OUTPUT
 aws serverlessrepo create-application-version \
   --application-id $APPLICATION_ID \
   --semantic-version $AWS_SEMANTIC_VERSION \
-  --source-code-url https://github.com/sandupotter/smartthing-serverless \
+  --source-code-url $SOURCE_URL \
   --template-body "file://"$SAM_TEMPLATE_OUTPUT
 
 echo "Deployed app $APPLICATION_ID with version $AWS_SEMANTIC_VERSION"

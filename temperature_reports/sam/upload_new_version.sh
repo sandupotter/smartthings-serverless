1cac142d31dfdb59ba398341dc810a0a98afd5f2
Sandu Potter <sandupotter@gmail.com>
2018-05-11 08:28:31 +0100

    SAM deployment script changes

--- temperature_reports/sam/upload_new_version.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-BASEDIR=$(dirname "$0")
-$BASEDIR/../../upload_new_version_to_serverless_repo.sh temperature_reports/sam/temperature_level.yaml temperature_reports/sam/temperature_level_template.yaml
--- upload_new_version_to_serverless_repo.sh
+++ upload_new_version_to_serverless_repo.sh
@@ -1,12 +1,55 @@
 #!/usr/bin/env bash
+
+function get_next_tag()
+{
+  latest_tag=`git describe --abbrev=0 --tags`
+  tag_tokens=$(echo $latest_tag | tr "." "\n")
+
+  next_tag=""
+  current_token_index=1
+  for token in $tag_tokens
+  do
+    if [ $current_token_index == "3" ]
+    then
+      token="$(($token+1))"
+      next_tag=$next_tag$token
+    else
+      next_tag=$next_tag$token"."
+    fi
+    current_token_index="$(($current_token_index+1))"
+  done
+  echo $next_tag
+}
+
 BASEDIR=$(dirname "$0")
-#python $BASEDIR/setup.py ldist
+python $BASEDIR/setup.py ldist
 SAM_TEMPLATE=$BASEDIR/$1
 SAM_TEMPLATE_OUTPUT=$BASEDIR/$2
-#aws cloudformation package --template-file $SAM_TEMPLATE --s3-bucket sandupotter-smart-things-serverless --output-template-file $SAM_TEMPLATE_OUTPUT
-sample_template_output_one_line=`tr -d '\n' < $SAM_TEMPLATE_OUTPUT`
+APPLICATION_ID=$3
+NEXT_TAG=$(get_next_tag)
+LATEST_TAG=`git describe --abbrev=0 --tags`
+
+if [ $# -eq 3 ]
+then
+  SEMANTIC_VERSION=$NEXT_TAG
+else
+  SEMANTIC_VERSION=$4
+fi
+
+if [ $LATEST_TAG != $SEMANTIC_VERSION ]
+then
+  echo "Pushing new tag $SEMANTIC_VERSION ..."
+  git tag $SEMANTIC_VERSION
+  git push origin $SEMANTIC_VERSION
+fi
+
+AWS_SEMANTIC_VERSION=`echo $SEMANTIC_VERSION | cut -c 2-`
+
+aws cloudformation package --template-file $SAM_TEMPLATE --s3-bucket sandupotter-smart-things-serverless --output-template-file $SAM_TEMPLATE_OUTPUT
 aws serverlessrepo create-application-version \
-  --application-id arn:aws:serverlessrepo:us-west-2:180953841693:applications/smart-things-temperature-level-report \
-  --semantic-version 1.0.1 \
+  --application-id $APPLICATION_ID \
+  --semantic-version $AWS_SEMANTIC_VERSION \
   --source-code-url https://github.com/sandupotter/smartthing-serverless \
   --template-body "file://"$SAM_TEMPLATE_OUTPUT
+
+echo "Deployed app $APPLICATION_ID with version $AWS_SEMANTIC_VERSION"
